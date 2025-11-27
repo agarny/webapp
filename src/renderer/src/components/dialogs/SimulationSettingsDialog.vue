f85d50979cedaf13445bdb5d3654dbe5f394a6cf
Alan Garny <agarny@hellix.com>
2025-11-28 10:18:12 +1300

    simpropertyeditor

--- src/renderer/src/components/dialogs/SimulationSettingsDialog.vue
+++ src/renderer/src/components/dialogs/SimulationSettingsDialog.vue
@@ -1,5 +1,6 @@
 <template>
   <BaseDialog header="Simulation Settings...">
+    <SimulationConfiguration v-model="simulationData" />
     <template #footer>
       <Button autofocus label="OK" @click="$emit('ok')" />
       <Button label="Cancel" severity="secondary" @click="$emit('close')" />
@@ -8,5 +9,18 @@
 </template>
 
 <script setup lang="ts">
+import { SimulationConfiguration } from 'simpropertyeditor';
+import 'simpropertyeditor/simpropertyeditor.css'
+import * as vue from 'vue';
+
 defineEmits(['close', 'ok']);
+
+const simulationData = vue.ref({
+  input: [],
+  output: {
+    data: [],
+    plots: []
+  },
+  parameters: []
+});
 </script>
